7fd5a0cf12e052105381eab9b57f338032fc4662
Ronak Shah <3248487+shahronak@users.noreply.github.com>
2024-01-22 13:47:57 -0800

    Created game function and it works! Finished project

--- game.js
+++ game.js
@@ -18,12 +18,6 @@ Play Round
 Loop 5 Play Rounds and keep score
 */
 
-let playerChoice;
-let computerChoice;
-let playerScore = 0;
-let computerScore = 0;
-
-
 // Randomly select a computer choice for rock, paper, and scissors
 function getComputerChoice() {
     switch (Math.floor(Math.random() * 3)) {
@@ -60,6 +54,43 @@ function playRound (playerSelection, computerSelection) {
     }
 }
 
+// Play a 5 round game of rock paper scissors and collect a player prompt for each round
 function game() {
+    let playerChoice;
+    let computerChoice;
+    let result;
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i < 5 ; i++) {
+        computerChoice = getComputerChoice();
+        playerChoice = prompt("Round " + (i+1) + ": Choose Rock, Paper, or Scissors");
+        result = playRound(playerChoice, computerChoice);
+        if (result == "tie") {
+            console.log ("You Tied!");
+        }
+        else if (result == "win") {
+            console.log ("You Win! " + playerChoice + " beats " + computerChoice);
+            playerScore++;
+        }
+        else if (result == "lose") {
+            console.log ("You Lose! " + computerChoice + " beats " + playerChoice);
+            computerScore++;
+        }
+        else {
+            console.log ("You entered an incorrect choice. Enter rock, paper, or scissors.")
+            i--;
+        }
+    }
 
+    if (playerScore > computerScore) {
+        console.log ("You win this game!");
+    }
+    else if (playerScore < computerScore) {
+        console.log ("You lose this game!");
+    }
+    else {
+        console.log ("You tied this game!")
+    }
 }
+
+game();
